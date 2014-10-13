03710afd0bdd0c310b867cf2ffc5be09aca4c73e
Martin Thøgersen <martin@webnuts.com>
2014-10-13 15:05:05 +0200

    Added license + NuGet packagespec

--- Honeybadger.ErrorReporter/Properties/AssemblyInfo.cs
+++ Honeybadger.ErrorReporter/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Honeybadger.ErrorReporter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Honeybadger error reporting integration")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Webnuts ApS")]
 [assembly: AssemblyProduct("Honeybadger.ErrorReporter")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
